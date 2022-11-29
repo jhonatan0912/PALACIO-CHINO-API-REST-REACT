6aa2ec065a98096a41165e64b00ca6f6c32ced0a
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-11-28 22:28:50 -0500

    change products.api.js URL

--- client/src/api/products.api.js
+++ client/src/api/products.api.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 export const createProductRequest = async (product) => {
-  return await axios.post('https://palacio-chino-back-production.up.railway.app/api/products', product);
+  return await axios.post('http://localhost:5173/', product);
 }
 
 export const getProductsRequest = async () => {
-  return await axios.get('https://palacio-chino-back-production.up.railway.app/api/products')
+  return await axios.get('http://localhost:5173/')
 }
 
 export const getProductRequest = async (id) => {
-  return await axios.get(`https://palacio-chino-back-production.up.railway.app/api/products/${id}`)
+  return await axios.get(`http://localhost:5173/${id}`)
 }
 
 export const updateProductRequest = async (id, newFields) => {
-  return await axios.patch(`https://palacio-chino-back-production.up.railway.app/api/products/${id}`, newFields);
+  return await axios.patch(`http://localhost:5173/${id}`, newFields);
 }
 
 export const deleteProductRequest = async (id) => {
-  return await axios.delete(`https://palacio-chino-back-production.up.railway.app/api/products/${id}`);
+  return await axios.delete(`http://localhost:5173/${id}`);
 }
