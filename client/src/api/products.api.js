34b631d0416e3a8ff93d6910bee9738e8435a5f3
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-01-02 11:53:39 -0500

    products urls changed

--- client/src/api/products.api.js
+++ client/src/api/products.api.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 
 export const createProductRequest = async (product) => {
-  return await axios.post('http://localhost:5173/', product);
+  return await axios.post('https://palacio-chino-back-production.up.railway.app/', product);
 }
 
 export const getProductsRequest = async () => {
-  return await axios.get('http://localhost:5173/')
+  return await axios.get('https://palacio-chino-back-production.up.railway.app/')
 }
 
 export const getProductRequest = async (id) => {
-  return await axios.get(`http://localhost:5173/${id}`)
+  return await axios.get(`https://palacio-chino-back-production.up.railway.app/${id}`)
 }
 
 export const updateProductRequest = async (id, newFields) => {
-  return await axios.patch(`http://localhost:5173/${id}`, newFields);
+  return await axios.patch(`https://palacio-chino-back-production.up.railway.app/${id}`, newFields);
 }
 
 export const deleteProductRequest = async (id) => {
-  return await axios.delete(`http://localhost:5173/${id}`);
+  return await axios.delete(`https://palacio-chino-back-production.up.railway.app/${id}`);
 }
