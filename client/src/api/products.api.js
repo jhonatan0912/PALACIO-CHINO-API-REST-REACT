4aaaa8f55210af1876412677b852a08b8ae66751
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-01-02 12:00:44 -0500

    test urls updated

--- client/src/api/products.api.js
+++ client/src/api/products.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const createProductRequest = async (product) => {
-  return await axios.post('https://palacio-chino-back-production.up.railway.app/', product);
+  return await axios.post('https://palacio-chino-back-production.up.railway.app/api/products', product);
 }
 
 export const getProductsRequest = async () => {
@@ -9,13 +9,13 @@ export const getProductsRequest = async () => {
 }
 
 export const getProductRequest = async (id) => {
-  return await axios.get(`https://palacio-chino-back-production.up.railway.app/${id}`)
+  return await axios.get(`https://palacio-chino-back-production.up.railway.app/api/products/${id}`)
 }
 
 export const updateProductRequest = async (id, newFields) => {
-  return await axios.patch(`https://palacio-chino-back-production.up.railway.app/${id}`, newFields);
+  return await axios.patch(`https://palacio-chino-back-production.up.railway.app/api/products/${id}`, newFields);
 }
 
 export const deleteProductRequest = async (id) => {
-  return await axios.delete(`https://palacio-chino-back-production.up.railway.app/${id}`);
+  return await axios.delete(`https://palacio-chino-back-production.up.railway.app/api/products/${id}`);
 }
